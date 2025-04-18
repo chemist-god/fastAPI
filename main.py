5810ca46cbccae2bc38518cf23ca29d1653111b4
chemist-god <godtenkorang72@gmail.com>
2025-04-18 23:59:45 +0000

    commit main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
@@ -6,4 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello FastAPI"}
 
-    
+    @app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
