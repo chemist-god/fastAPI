ac878289760f58747daef15c46965f692e4dee94
chemist-god <godtenkorang72@gmail.com>
2025-04-19 23:30:06 +0000

    get-method

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI
 from typing import Union
 
 app = FastAPI()
-
+ 
+ 
 @app.get("/")
 async def root():
-    return {"message": "Hello FastAPI"}
+    return {"message": "Hello to my FastAPI"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+   ## review this later on
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+@app.get("/post")
+def get_post():
+    return {"data": "This is your post"}
