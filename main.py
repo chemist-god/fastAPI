1dbed0c56fcd56fc7cb753fda353766c9dc31e18
chemist-god <godtenkorang72@gmail.com>
2025-04-21 23:58:01 +0000

    fixed

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from typing import Union
-from fastapi.params import Body
 from pydantic import BaseModel
 
 app = FastAPI()
- 
+
 # Pydantic model for request body validation
 class Post(BaseModel):
     title: str
@@ -12,22 +11,19 @@ class Post(BaseModel):
     published: bool = True
     rating: float = None
 
-    my_post =[{"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "favorite food", "content": "I love Burger", "id":2}   ]
+# Define my_post outside the Post class
+my_post = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "favorite food", "content": "I love Burger", "id": 2}
+]
 
 @app.get("/")
 async def root():
     return {"message": "Hello to my FastAPI"}
 
-   ## review this later on
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
 @app.get("/post")
 def get_post():
     return {"data": my_post}
-
 
 # POST REQUEST
 @app.post("/createpost")
